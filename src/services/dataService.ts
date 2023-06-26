24c9abbc8278eb74ac3922d8fa364a9de0c2dc27
matiasfeliu92 <matumazparrote@gmail.com>
2023-06-26 17:46:51 -0300

    refactor: add get all artists and get all genres methods in datasService file, and their interfaces for data types

--- src/services/dataService.ts
+++ src/services/dataService.ts
@@ -8,4 +8,12 @@ export default class DataService {
     async newSong (title: string, artist: string, album: string, duration: string, genre: string, image: string, you_tube: string) {
         return axiosConfig.post("/songs", {title, artist, album, duration, genre, image, you_tube})
     }
+
+    async getAllArtists () {
+        return axiosConfig.get("/artists")
+    }
+
+    async getAllGenres () {
+        return axiosConfig.get("/genres")
+    }
 } 
